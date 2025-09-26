ed2851045890bddd0539ecb6d3653edca0a81af4
yamini <yamini.com>
2025-09-26 09:35:57 +0530

    robo

--- roboshop.sh
+++ roboshop.sh
@@ -10,10 +10,10 @@ do
     Instance_Id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instace != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        Record_Name= "$instance.$domain_name"
+        Record_Name="$instance.$domain_name"
     else
         IP=$(aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        Record_Name= "$domain_name"
+        Record_Name="$domain_name"
     fi 
     echo "$instance : $IP"
 
