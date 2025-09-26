4b2e3b4df71927a39e1fed33be23661c297f7590
yamini <yamini.com>
2025-09-26 09:31:55 +0530

    robo

--- roboshop.sh
+++ roboshop.sh
@@ -2,15 +2,38 @@
 
 ami_id="ami-09c813fb71547fc4f"
 sg_id="sg-0e9e29a7d8854c36b"
+zone_id="Z06672831LSN6WUF978LV"
+domain_name="yaminiaws.fun"
 
 for instance in $@
 do
     Instance_Id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instace != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        Record_Name= "$instance.$domain_name"
     else
         IP=$(aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        Record_Name= "$domain_name"
     fi 
     echo "$instance : $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $zone_id \
+    --change-batch '
+    {
+        "Comment": "updating record set"
+        ,"Changes": [{
+        "Action"              : "upsert"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$Record_Name'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
 
