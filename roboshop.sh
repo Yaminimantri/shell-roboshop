b017ef18ec1fc1c6fdda32980d825a758b9a8c1e
yamini <yamini.com>
2025-09-26 08:41:35 +0530

    robo

--- roboshop.sh
+++ roboshop.sh
@@ -7,9 +7,9 @@ for instance in $@
 do
     Instance_Id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instace != "frontend" ]; then
-        IP=$(aws ec2 describe-instance --instance-ids $Instance_Id --query 'Reservations[0].Instances[0]PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        IP=$(aws ec2 describe-instance --instance-ids $Instance_Id --query 'Reservations[0].Instances[0]PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi 
     echo "$instance : $IP"
 done
